b511435b38ee7217fc87344f549590b4f613273b
Jadgesh <jadgesh.i@gmail.com>
2024-10-14 20:38:41 -0400

    added character tracking feature

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ def main():
     # Read the file and return the data
     book_text = read_book(file_path)
     word_count = count_words(book_text)
+    char_dict = count_characters(book_text)
     # Debugging code remove me
     print(f"{word_count} total words")
+    print(char_dict)
     
 def read_book(path):
     book_text = None
@@ -21,5 +23,19 @@ def read_book(path):
 def count_words(input):
     return len(input.split())
 
+def count_characters(input):
+    char_dict = {}
+    
+    for char in input:
+        if not(char.isalpha()):
+            continue
+        
+        lower_case = char.lower()
+        if lower_case in char_dict:
+            char_dict[lower_case] += 1
+        else:
+            char_dict[lower_case] = 1
+    
+    return char_dict
 if __name__ == '__main__':
     main()
